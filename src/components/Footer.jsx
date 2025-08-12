fc4d78da7631ef3c4d1af38741527e26359d585e
dsilva1832 <alwindisilva2014@gmail.com>
2025-08-12 17:47:43 +0530

    flavorsome v4

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,31 +14,29 @@ const Footer = () => {
       {/* <h3 className='text-center text-light'>Contact Us</h3> */}
       <div className='p-5 row align-items-center justify-content-center'>
        <div className='col-lg-6'>
-        <div className="col-lg-2"></div>
-        <div className='col-lg-8'>
+        {/* <div className="col-lg-2"></div> */}
+        {/* <div className='col-lg-8 mt-5'> */}
           <h4 className='text-light'>Signup to our Newsletter</h4>
           <TextField id="filled-basic1" label="email" variant="filled" />
-          <button type='btn' className='btn btn-primary ms-3 py-3 px-3'>Sign in</button>
-        </div>
-        <div className="col-lg-2"></div>
+          <button type='btn' className='btn btn-primary ms-2 py-3 px-3'>Sign in</button>
+        {/* </div> */}
+        {/* <div className="col-lg-2"></div> */}
        </div>
 
        <div className='col-lg-6 icon'>
-        <div className="col-lg-2"></div>
-        <div className='col-lg-8'>
+        {/* <div className="col-lg-2"></div> */}
+        {/* <div className='col-lg-8 mt-5'> */}
           <h3 className='text-light'>Follow Us</h3>
           <a href="https://www.youtube.com/" target="_blank" className='text-light'><FaYoutube className='me-5'/></a>
           <a href='https://www.instagram.com/' target='_blank' className='text-light'><FaInstagram className='me-5'/></a>
           <a href='https://www.facebook.com/' target='_blank' className='text-light'><FaFacebookF className='me-5'/></a>
           <a href='https://www.x.com/' target='_blank' className='text-light'><FaXTwitter /></a>
-        </div>
-        <div className="col-lg-2"></div>
+        {/* </div> */}
+        {/* <div className="col-lg-2"></div> */}
        </div>
       </div>
-
-      <p className='text-center text-light'>Flavorsome - August 2025 Alwin Joseph Dsilva</p>
-
-
+      <p className='text-center text-light mb-0 mt-5' id="footer">&copy;Flavorsome-August 2025, All Rights Reserved to &reg;Alwin Joseph Dsilva</p>
+      <p className='text-center text-light mb-0 mt-2'>Build with ❤️ using React</p>    
     </div>
   )
 }
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,11 +1,44 @@
 import React from 'react'
+import './Landing.css'
 
 const Landing = () => {
   return (
     <div>
-      <section>
-        landing page
+      <section className='bg1'>
+        <div className='row'>
+          <div className='col-lg-7'></div>
+          <div className='col-lg-5'>
+            <h3 className='name text-light'>Flavorsome</h3>
+            <p className='text-light'>Flavour, Perfected</p>
+          </div>
+        </div>
       </section>
+
+      {/* top dishes */}
+      <section id='dishes'>
+        <h2 className='text-center mt-3'>Top Dishes</h2>
+        <div className='d-flex justify-content-center align-items-evenly'>
+          <div className='row px-5'>
+            <div className='col-4'>
+              <img src="https://images.pexels.com/photos/5807020/pexels-photo-5807020.jpeg" alt="no img" className='img-fluid'/>
+              <p style={{background:'#fde33d'}}>Pasta</p>
+            </div>
+            <div className='col-4'>
+            <img src="https://images.pexels.com/photos/5807020/pexels-photo-5807020.jpeg" alt="no img" className='img-fluid'/>
+            <p>Pasta</p>
+            </div>
+            <div className='col-4'>
+            <img src="https://images.pexels.com/photos/5807020/pexels-photo-5807020.jpeg" alt="no img" className='img-fluid'/>
+            <p>Pasta</p>
+            </div>
+  
+          </div>
+        </div>
+
+      </section>
+
+
+
     </div>
   )
 }
